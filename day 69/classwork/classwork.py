0d1fd73bdf9003fabf2d3da8f4b59c3d2e2f2ea6
MatePiranishvili <158074013+MatePiranishvili@users.noreply.github.com>
2025-07-05 19:55:17 +0400

    commit
    Goa

--- day 69/classwork/classwork.py
+++ day 69/classwork/classwork.py
@@ -1,7 +1,8 @@
-def split_it( ):
-    hi = "hello my name is mate"
-    hi = hi.upper()
-    hi = hi.join("")
-    print(hi)
-split_it( )
+def capitalize_letter(word):
+    word  = word .split(" ")
+    a = []
+    for i in word:
+        a.append(i.capitalize())
+    return "".join(a)
+print(capitalize_letter("hello my name is mate"))
     
